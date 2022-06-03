923f5e9f7127182cb37150339541452726f366c3
Matt <matt@excalamus.com>
2022-06-02 22:12:51 -0400

    v1.2.0
    * Implement Configuration.set to apply configuration settings
    * Implement Configuration.get to update configuration settings
    * Fix premature shell exit for build script
    * Fix debug toggle
    * Refactor tests
    * Update documentation

--- setup.py
+++ setup.py
@@ -10,6 +10,8 @@ import os
 from setuptools import setup, find_packages
 
 
+VERSION = "1.2.0"
+
 with open("README.md", 'r', encoding="utf-8") as f:
     LONG_DESCRIPTION = f.read()
 
@@ -37,7 +39,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/guides/single-sourcing-package-version/
-    version="1.0.3",  # Required
+    version=VERSION,  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
